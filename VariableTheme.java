fbf5576d9a3b517a2507e4c2c8d1d79d74fec6b5
Leonid_Makhov <makhov-leonid999@yandex.ru>
2023-10-30 22:31:12 +0300

    1s

---
+++ VariableTheme.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class VariableTheme {
+    public static void main(String[] arg) {
+        int num = 2;
+        System.out.println("1. цифра = " + num + "\n");
+
+        //сумма чисел
+        int sum = 40 + 14;
+        System.out.println("2. сумма чисел = " + sum + "\n");
+    }
+}
