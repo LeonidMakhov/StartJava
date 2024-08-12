a6fdc774dc52ee9d4e59b15f733a2d2f8df80976
Leonid_Makhov <makhov-leonid999@yandex.ru>
2024-08-12 22:58:13 +0300

    add 2 files

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,17 +1,17 @@
-public class CyclesTheme {
+public class MyFirstGame {
     public static void main(String[] args) {
-        System.out.println("1. Подсчет суммы четных и нечетных чисел");
-        int a = -10;
-        int b = 21;
-        int sumEvenCounts = 0;
-        int sumOddCounts = 0;
-        do {
-            if (a % 2 == 0) {
-                sumEvenCounts = sumEvenCounts + a;
-            } else if (a % 2 != 0) {
-                sumOddCounts = sumOddCounts + a;
+        int computerNumber = 50;
+        int myMumber = 70;
+        while (computerNumber != myMumber) { //while (true)
+            if (myMumber > computerNumber) {
+                System.out.println(myMumber + " больше того, что загадал компьютер");
+                myMumber--;
+            } 
+            if (myMumber < computerNumber) {
+                System.out.println(myMumber + " меньше того, что загадал компьютер");
+                myMumber++;
             }
-            a++;
-        } while (a <= b);
-        System.out.println("В отрезке [-10, 21] сумма четных чисел = " + sumEvenCounts + 
-                " , a нечетных = " + sumOddCounts);
+        }
+        System.out.println("Вы победили!");
+    }
+}
--- Lesson_1/RpsGameFormatting.java
+++ Lesson_1/RpsGameFormatting.java
@@ -1,17 +1,74 @@
-public class CyclesTheme {
-    public static void main(String[] args) {
-        System.out.println("1. Подсчет суммы четных и нечетных чисел");
-        int a = -10;
-        int b = 21;
-        int sumEvenCounts = 0;
-        int sumOddCounts = 0;
-        do {
-            if (a % 2 == 0) {
-                sumEvenCounts = sumEvenCounts + a;
-            } else if (a % 2 != 0) {
-                sumOddCounts = sumOddCounts + a;
+package ru.topjava.lesson1;
+
+import java.util.Random;
+
+public class RpsGameFormatting {
+    // Игра Камень-Ножницы-Бумага
+    public static void main(String[] args) throws InterruptedException {
+        String rock = "R";
+        String scissors = "S";
+        String paper = "P";
+        Random r = new Random();
+
+        // Ход первого игрока
+        String name1 = "HEL";
+        int position1 = r.nextInt(1, 100);
+        String sign1 = rock;
+        if (position1 > 66) {
+            sign1 = paper;
+        } else if (position1 > 33) {
+            sign1 = scissors;
+        }
+
+        System.out.println("Ход " + name1 + ": ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
+        System.out.println(sign1);
+
+        // Ход второго игрока
+        String name2 = "WALLE";
+        position1 = r.nextInt(1, 100);
+        String sign2 = rock;
+        if (position1 > 66) {
+            sign2 = paper;
+        } else {
+            if (position1 > 33) {
+                sign2 = scissors;
             }
-            a++;
-        } while (a <= b);
-        System.out.println("В отрезке [-10, 21] сумма четных чисел = " + sumEvenCounts + 
-                " , a нечетных = " + sumOddCounts);
+        }
+
+        System.out.println("Ход " + name2 + ": ");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(rock + "\r");
+            Thread.sleep(100);
+            System.out.print(scissors + "\r");
+            Thread.sleep(100);
+            System.out.print(paper + "\r");
+            Thread.sleep(100);
+        }
+
+        System.out.println(sign2);
+
+        if (sign1.equals(sign2)) {
+            System.out.println("Победила дружба!");
+            return;
+        }
+        boolean isEqualName1 = sign1.equals(rock) && sign2.equals(scissors) ||
+                    sign1.equals(scissors) && sign2.equals(paper) ||
+                    sign1.equals(paper) && sign2.equals(rock);
+
+        if (isEqualName1) {
+            System.out.println("\nПобедил - " + name1);
+        } else {
+            System.out.println("\nПобедил - " + name2);
+        }
+    }
+}
